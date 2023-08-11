65aa290c23121e14ef610cef33b99002cd781828
Inas <inasbani@outlook.com>
2023-08-10 20:04:01 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main function - This Program Prints the message below using puts function
+ * main - This Program Prints the message below using puts function
  * Return: 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
