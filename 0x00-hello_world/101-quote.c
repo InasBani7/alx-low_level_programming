d5ede5398464be92cd1f20bdcb911a41df7c18f0
Inas <inasbani@outlook.com>
2023-08-11 09:22:21 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdout.h>
+#include <unistd.h>
 /**
  * main - Entry Point
  * Description: This program prints the below quote using fput function.
@@ -8,6 +8,6 @@
  */
 int main(void)
 {
-	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
