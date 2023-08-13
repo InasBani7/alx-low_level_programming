61bf76193568594bbf57added8c2cd96c969e6bf
Inas <inasbani@outlook.com>
2023-08-13 13:02:39 -0700

    task0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,18 @@
+#include<stdio>
+/*
+ * main - Entry Point
+ * Description: detects if n is -ve or +ve and prints out a message accoringly
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	if (n < o)
+		fprint("is negative");
+	else if (n > 0)
+		fprint("is negative");
+	else
+		fprint("is zero");
+	return (0);
+}
