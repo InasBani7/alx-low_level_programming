802a53d973c42840bf725f34d3e8d8327a5afd64
Inas <inasbani@outlook.com>
2023-08-13 11:38:41 -0700

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,6 +10,7 @@ int main(void)
 {
 	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
-	fputs("quote\n", stdout);
+	fputs(quote, stdout);
+	fputs("\n", stdout);
 	return (1);
 }
