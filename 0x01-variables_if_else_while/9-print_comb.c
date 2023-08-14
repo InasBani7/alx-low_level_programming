b99394ee354ba8a8a017e228c7d653d4f4ec080f
Inas <inasbani@outlook.com>
2023-08-14 14:22:06 -0700

    task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Description: print numbers from 0 to 9 seperated by ", "
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(48 + i);
+		putchar(", ");
+		i++;
+	}
+	return (0);
+}
