8251a8d6be2d5e23b05c6f446f4bb1e176736644
Inas <inasbani@outlook.com>
2023-08-15 04:07:14 -0700

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Description: This program prints _putchar to the standard output.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\_putchar");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This Directory contains my work for the project 0x02-functions_nested_loops
