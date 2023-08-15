099f89357cc913805227816868a818ec5e842871
Inas <inasbani@outlook.com>
2023-08-15 12:17:33 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include<main.h>
+
+/**
+ * print_alphabet - using the user defind function _putchar
+ * to print lowercase alphabets
+ */
+void print_alphabet(void)
+{
+	int alpha;
+
+	for (alpha = 'a'; alpha <= 'z' ; alpha++)
+		_putchar(ch);
+	_putchar('\n');
+}
