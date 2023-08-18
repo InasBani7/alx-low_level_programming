7e5cb40a91f737159ea2fc552539439f5a3895e7
Inas <inasbani@outlook.com>
2023-08-17 20:22:04 -0700

    task100

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+/**
+ * _squareroot - finds out the square root of the given number
+ * @y: Function Input
+ * return: The squareroot of the given number
+ */
+
+double _squareroot(double y)
+{
+	float sqrt, z;
+
+	sqrt = y / 2;
+	z = 0;
+
+	while (sqrt != z)
+	{
+		z = sqrt;
+		sqrt = (y / z + z) / 2;
+	}
+
+	return (sqrt);
+}
+
+
+/**
+ * largest_prime_factor - finds out the largest prime factor of x
+ * @x: Function input
+ */
+
+void largest_prime_factor(long int x)
+{
+	int prime, largest;
+
+	/* divide the smallest prime number by 2 */
+	while (x % 2 == 0)
+		x = x / 2;
+	prime = 3;
+	while (prime <= _squareroot(num))
+	{
+		while (x % prime == 0)
+		{
+			x = x / prime;
+			largest = prime;
+		}
+		prime = prime + 2;
+	}
+	if (x > 2)
+		largest = num;
+	printf("%d\n", largest);
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	largest_prime_factor(612852475143);
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+README file for the pproject 0x04-more_functions_nested_loops
