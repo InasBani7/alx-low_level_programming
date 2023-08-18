bed1835a2d28bd583f08a3f8e9d3cb12a3b659b6
Inas <inasbani@outlook.com>
2023-08-18 16:47:12 -0700

    task6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,11 +6,14 @@
  */
 void print_line(int n)
 {
-	if (n == 0)
+	int i;
+
+	if (n <= 0)
 		_putchar('\n');
 	else
-		for (int i = 0; i < n; i++)
+		for (i = 0; i < n; i++)
 		{
 			_putchar('_');
+			_putchar('\n');
 		}
 }
