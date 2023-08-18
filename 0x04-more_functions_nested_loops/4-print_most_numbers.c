9ef045d301b8ccaa5dc069806958d02c19451824
Inas <inasbani@outlook.com>
2023-08-18 15:50:27 -0700

    tash4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,10 +7,11 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	ifor (i = 0; i < 10; i++)
 	{
-		if (i != 2 || i != 4)
-			_putchar(i + 48);
+		if (i == 2 || i == 4)
+			continue;
+		_putchar(i + 48);
 	}
 	_putchar('\n');
 }
