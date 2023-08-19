3419e0d03378f681fe7882d85bda6f989844da1c
Inas <inasbani@outlook.com>
2023-08-18 18:21:41 -0700

    task9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,18 +14,19 @@ int main(void)
 	num = 0;
 	while (num < 100)
 	{
-		if (num % 3 == 0 && num % 5 == 0)
-			printf("FizzBuzz");
-		else if (num % 3 == 0 && !(num % 5 == 0))
+		if (num % 3 == 0 && num % 5 != 0)
 			printf("Fizz");
-		else if (!(num % 3 == 0) && num % 5 == 0)
+		else if (num % 3 != 0 && !(num % 5 == 0))
 			printf("Buzz");
+		else if (!(num % 3 == 0) && num % 5 == 0)
+			printf("FizzBuzz");
 		else
 			printf("%d", num);
 		if (num != 100)
 			printf(" ");
 		else
 			printf("\n");
+		num++;
 	}
 
 	return (0);
