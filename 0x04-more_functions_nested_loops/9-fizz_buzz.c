14b30b52a7a89b049d7d9726a36dbdd63d951a6a
Inas <inasbani@outlook.com>
2023-08-18 18:00:03 -0700

    task9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints the numbers from 1 to 100, or
+ * multiples of three print Fizz,
+ * for the multiples of five print Buzz
+ * For numbers which are multiples of both three and five print FizzBuzz.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	num = 0;
+	while (num < 100)
+	{
+		if (num % 3 == 0 && num % 5 == 0)
+			printf("FizzBuzz");
+		else if (num % 3 == 0 && !(num % 5 == 0))
+			printf("Fizz");
+		else if (!(num % 3 == 0) && num % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", num);
+		if (num != 100)
+			printf(" ");
+		else
+			printf("\n");
+	}
+
+	Return(0);
+}
