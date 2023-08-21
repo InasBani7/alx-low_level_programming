1feb4dd959b012a9ed983d1f2e95a428162f27d8
Inas <inasbani@outlook.com>
2023-08-21 14:48:59 -0700

    task6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,13 +7,12 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i = i + 2)
 	{
 		
 		_putchar(str[i]);
-		i = i + 2;
 	
 	}
 	_putchar('\n');
