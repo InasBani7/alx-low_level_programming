d09264f383dbc42dcb8edf072ea256849ffd228c
Inas <inasbani@outlook.com>
2023-08-21 12:40:28 -0700

    task0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of the input pointer to 98.
+ * @n: Input Parameter
+ */
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+README file for the project 0x05-pointers_arrays_strings
